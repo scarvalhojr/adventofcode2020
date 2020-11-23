237429b23c7563c3070d54d3b305e14b897e598c
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-11-23 20:35:11 +0000

    Add scaffold for all days

---
+++ day08/src/main.rs
@@ -0,0 +1,33 @@
+use clap::{crate_description, App, Arg};
+use day08::{part1, part2};
+use std::fs::File;
+use std::io::BufReader;
+use std::process::exit;
+
+fn main() {
+    let args = App::new(crate_description!())
+        .arg(
+            Arg::with_name("INPUT")
+                .help("File with puzzle input")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    println!(crate_description!());
+    let _input = read_input(args.value_of("INPUT").unwrap());
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
+
+fn read_input(filename: &str) -> i32 {
+    let _file = match File::open(filename) {
+        Ok(file) => BufReader::new(file),
+        Err(err) => {
+            println!("Failed to open file '{}': {}", filename, err.to_string());
+            exit(2);
+        }
+    };
+
+    unimplemented!()
+}
