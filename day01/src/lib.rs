4572925187aa28eb7430b4c134d6102dc36b544a
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-01 19:27:32 +0000

    Day 1

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,7 +1,25 @@
-pub fn part1() -> i32 {
-    unimplemented!()
+use std::collections::HashSet;
+
+pub fn part1(input: &[i32]) -> Option<i32> {
+    let target = 2020;
+    let items = input.iter().collect::<HashSet<_>>();
+
+    // All entries in the input are unique
+    items.iter().find_map(|&&item1| {
+        items.get(&(target - item1)).map(|&&item2| item1 * item2)
+    })
 }
 
-pub fn part2() -> i32 {
-    unimplemented!()
+pub fn part2(input: &[i32]) -> Option<i32> {
+    let target = 2020;
+    let items = input.iter().collect::<HashSet<_>>();
+
+    // All entries in the input are unique
+    items.iter().find_map(|&&item1| {
+        items.iter().find_map(|&&item2| {
+            items
+                .get(&(target - item1 - item2))
+                .map(|&&item3| item1 * item2 * item3)
+        })
+    })
 }
