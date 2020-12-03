7451829143d4bb00cfe62f81b910d897557d9955
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-03 18:45:54 +0000

    Day 3

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,7 +1,67 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+use std::collections::HashSet;
+use std::str::FromStr;
+
+type Position = (usize, usize);
+type Slope = (usize, usize);
+
+pub struct TreeMap {
+    width: usize,
+    height: usize,
+    trees: HashSet<Position>,
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+impl TreeMap {
+    fn count_hits(&self, slope: &Slope) -> usize {
+        let mut position = Position::default();
+        let mut hits = 0;
+
+        while position.1 < self.height {
+            if self.trees.contains(&position) {
+                hits += 1;
+            }
+            position = self.slide(&position, slope);
+        }
+        hits
+    }
+
+    fn slide(&self, position: &Position, slope: &Slope) -> Position {
+        ((position.0 + slope.0) % self.width, position.1 + slope.1)
+    }
+}
+
+pub fn part1(treemap: &TreeMap) -> usize {
+    treemap.count_hits(&(3, 1))
+}
+
+pub fn part2(treemap: &TreeMap) -> usize {
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|slope| treemap.count_hits(slope))
+        .product()
+}
+
+impl FromStr for TreeMap {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trees = s
+            .lines()
+            .enumerate()
+            .flat_map(|(y_coord, line)| {
+                line.chars().enumerate().filter(|(_, ch)| *ch != '.').map(
+                    move |(x_coord, ch)| match ch {
+                        '#' => Ok((x_coord, y_coord)),
+                        _ => Err(format!("Invalid character in map: {}", ch)),
+                    },
+                )
+            })
+            .collect::<Result<HashSet<_>, _>>()?;
+        let width = s.lines().map(|line| line.len()).max().unwrap_or(0);
+        let height = s.lines().count();
+        Ok(Self {
+            width,
+            height,
+            trees,
+        })
+    }
 }
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,8 +1,6 @@
 use clap::{crate_description, App, Arg};
-use day03::{part1, part2};
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::num::ParseIntError;
+use day03::*;
+use std::fs::read_to_string;
 use std::process::exit;
 
 fn main() {
@@ -17,7 +15,7 @@ fn main() {
 
     println!(crate_description!());
 
-    let input = match read_input(args.value_of("INPUT").unwrap()) {
+    let treemap = match read_input(args.value_of("INPUT").unwrap()) {
         Ok(data) => data,
         Err(err) => {
             println!("Failed to read input: {}", err);
@@ -25,31 +23,12 @@ fn main() {
         }
     };
 
-    match part1(&input) {
-        Some(result) => println!("Part 1: {}", result),
-        None => println!("Part 1: not found"),
-    };
-
-    match part2(&input) {
-        Some(result) => println!("Part 2: {}", result),
-        None => println!("Part 2: not found"),
-    };
+    println!("Part 1: {}", part1(&treemap));
+    println!("Part 2: {}", part2(&treemap));
 }
 
-fn read_input(filename: &str) -> Result<Vec<i32>, String> {
-    let input_file = File::open(filename).map_err(|err| err.to_string())?;
-
-    BufReader::new(input_file)
-        .lines()
-        .zip(1..)
-        .map(|(line, line_num)| {
-            line.map_err(|err| (line_num, err.to_string()))
-                .and_then(|value| {
-                    value.parse().map_err(|err: ParseIntError| {
-                        (line_num, err.to_string())
-                    })
-                })
-        })
-        .collect::<Result<_, _>>()
-        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
+fn read_input(filename: &str) -> Result<TreeMap, String> {
+    read_to_string(filename)
+        .map_err(|err| err.to_string())
+        .and_then(|s| s.parse())
 }
