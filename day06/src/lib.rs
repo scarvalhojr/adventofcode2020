dde334f150f74449d51bd09822b98ccfe7eb7d5e
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-06 11:31:49 +0000

    Day 6

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,7 +1,34 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+use std::collections::HashSet;
+
+pub type Group = Vec<Answer>;
+pub type Answer = HashSet<char>;
+
+pub fn part1(groups: &[Group]) -> usize {
+    groups
+        .iter()
+        .map(|group| {
+            group
+                .iter()
+                .fold(HashSet::new(), |all, answer| {
+                    all.union(answer).cloned().collect()
+                })
+                .len()
+        })
+        .sum()
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+pub fn part2(groups: &[Group]) -> usize {
+    groups
+        .iter()
+        .map(|group| {
+            // fold_first would be perfect here (available in nightly only)
+            let mut answers = group.iter();
+            let first = answers.next().cloned().unwrap_or_default();
+            answers
+                .fold(first, |all, answer| {
+                    all.intersection(answer).cloned().collect()
+                })
+                .len()
+        })
+        .sum()
 }
