f43256e6565849c2e9c1ea7dce062a1b322f688a
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-15 18:13:31 +0000

    Day 15

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,7 +1,27 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+use std::collections::HashMap;
+use std::convert::TryInto;
+
+fn find_nth_number(starting_numbers: &[i32], nth: i32) -> Option<i32> {
+    // Assumming there's no repetition in starting numbers!
+    let mut last_seen: HashMap<i32, i32> =
+        starting_numbers.iter().copied().zip(1..).collect();
+
+    let mut turn = starting_numbers.len().try_into().ok()?;
+    let mut number = *starting_numbers.iter().last()?;
+    while turn < nth {
+        number = match last_seen.insert(number, turn) {
+            Some(last_turn) => turn - last_turn,
+            _ => 0,
+        };
+        turn += 1;
+    }
+    Some(number)
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+pub fn part1(numbers: &[i32]) -> Option<i32> {
+    find_nth_number(numbers, 2020)
+}
+
+pub fn part2(numbers: &[i32]) -> Option<i32> {
+    find_nth_number(numbers, 30_000_000)
 }
