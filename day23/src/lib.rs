fc3b5ed31fbd366f8a4d8375b8e88a27b4aa9bfa
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-26 16:00:32 +0000

    Day 23

--- day23/src/lib.rs
+++ day23/src/lib.rs
@@ -1,7 +1,117 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+use std::str::FromStr;
+
+#[derive(Clone)]
+pub struct Cups {
+    max: usize,
+    current: usize,
+    next: Vec<usize>,
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+impl Cups {
+    fn do_move(&mut self) {
+        let pick1 = self.next[self.current];
+        let pick2 = self.next[pick1];
+        let pick3 = self.next[pick2];
+
+        let mut dest = self.decrement(self.current);
+        while dest == pick1 || dest == pick2 || dest == pick3 {
+            dest = self.decrement(dest)
+        }
+
+        self.next[self.current] = self.next[pick3];
+        self.next[pick3] = self.next[dest];
+        self.next[dest] = pick1;
+        self.current = self.next[self.current];
+    }
+
+    fn decrement(&self, number: usize) -> usize {
+        if number == 1 {
+            self.max
+        } else {
+            number - 1
+        }
+    }
+
+    fn get_labels(&self) -> String {
+        let mut labels = Vec::new();
+        let mut cup = self.next[1];
+        while cup != 1 {
+            labels.push(cup.to_string());
+            cup = self.next[cup];
+        }
+        labels.concat()
+    }
+
+    fn extend(&self, length: usize) -> Self {
+        if length < self.next.len() {
+            panic!("New length must be greater than current length");
+        }
+
+        let mut next = self.next.clone();
+        let mut next_elem = self.max + 1;
+        next.resize_with(length + 1, || {
+            next_elem += 1;
+            next_elem
+        });
+        next[length] = self.current;
+
+        let mut last = self.next[self.current];
+        while self.next[last] != self.current {
+            last = self.next[last];
+        }
+        next[last] = self.max + 1;
+
+        Self {
+            max: length,
+            current: self.current,
+            next,
+        }
+    }
+}
+
+pub fn part1(cups: &Cups) -> String {
+    let mut cups = cups.clone();
+    for _ in 1..=100 {
+        cups.do_move();
+    }
+    cups.get_labels()
+}
+
+pub fn part2(cups: &Cups) -> usize {
+    let mut cups = cups.extend(1_000_000);
+    for _ in 1..=10_000_000 {
+        cups.do_move();
+    }
+    let cup1 = cups.next[1];
+    let cup2 = cups.next[cup1];
+    cup1 * cup2
+}
+
+impl FromStr for Cups {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut labels = s
+            .trim()
+            .chars()
+            .map(|ch| {
+                ch.to_digit(10)
+                    .map(|digit| digit as usize)
+                    .ok_or_else(|| format!("Invalid label for cup '{}'", ch))
+            })
+            .collect::<Result<Vec<usize>, String>>()?;
+
+        // Assuming there are no gaps, no repetitions, no zeros
+        let max = *labels.iter().max().ok_or_else(|| "No label found")?;
+        let current = labels[0];
+        let mut next = vec![0; labels.len() + 1];
+        for window in labels.windows(2) {
+            match *window {
+                [num1, num2] => next[num1] = num2,
+                _ => unreachable!(),
+            }
+        }
+        next[labels.pop().unwrap()] = current;
+        Ok(Self { max, current, next })
+    }
 }
--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,8 +1,6 @@
 use clap::{crate_description, App, Arg};
 use day23::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::num::ParseIntError;
+use std::fs::read_to_string;
 use std::process::exit;
 
 fn main() {
@@ -17,7 +15,7 @@ fn main() {
 
     println!(crate_description!());
 
-    let input = match read_input(args.value_of("INPUT").unwrap()) {
+    let cups = match read_input(args.value_of("INPUT").unwrap()) {
         Ok(data) => data,
         Err(err) => {
             println!("Failed to read input: {}", err);
@@ -25,31 +23,12 @@ fn main() {
         }
     };
 
-    match part1(&input) {
-        Some(result) => println!("Part 1: {}", result),
-        None => println!("Part 1: not found"),
-    };
-
-    match part2(&input) {
-        Some(result) => println!("Part 2: {}", result),
-        None => println!("Part 2: not found"),
-    };
+    println!("Part 1: {}", part1(&cups));
+    println!("Part 2: {}", part2(&cups));
 }
 
-fn read_input(filename: &str) -> Result<Vec<i32>, String> {
-    let input_file = File::open(filename).map_err(|err| err.to_string())?;
-
-    BufReader::new(input_file)
-        .lines()
-        .zip(1..)
-        .map(|(line, line_num)| {
-            line.map_err(|err| (line_num, err.to_string()))
-                .and_then(|value| {
-                    value.parse().map_err(|err: ParseIntError| {
-                        (line_num, err.to_string())
-                    })
-                })
-        })
-        .collect::<Result<_, _>>()
-        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
+fn read_input(filename: &str) -> Result<Cups, String> {
+    read_to_string(filename)
+        .map_err(|err| err.to_string())?
+        .parse()
 }
