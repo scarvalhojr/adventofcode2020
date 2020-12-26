4bffa76eac9af2f21cd730b737a9d37d16c501f5
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-26 13:31:21 +0000

    Day 22

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,7 +1,86 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+use std::collections::{HashSet, VecDeque};
+
+pub type Cards = VecDeque<usize>;
+
+fn combat(player1: &mut Cards, player2: &mut Cards) {
+    while !player1.is_empty() && !player2.is_empty() {
+        let card1 = player1.pop_front().unwrap();
+        let card2 = player2.pop_front().unwrap();
+        if card1 > card2 {
+            player1.push_back(card1);
+            player1.push_back(card2);
+        } else {
+            player2.push_back(card2);
+            player2.push_back(card1);
+        }
+    }
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+fn score(cards: &Cards) -> usize {
+    cards
+        .iter()
+        .rev()
+        .zip(1..)
+        .map(|(card, weight)| card * weight)
+        .sum()
+}
+
+pub fn part1(cards1: &Cards, cards2: &Cards) -> usize {
+    let mut player1: Cards = cards1.clone();
+    let mut player2: Cards = cards2.clone();
+    combat(&mut player1, &mut player2);
+    let winner = if player1.is_empty() { player2 } else { player1 };
+    score(&winner)
+}
+
+enum Winner {
+    Player1,
+    Player2,
+}
+
+fn recursive_combat(player1: &mut Cards, player2: &mut Cards) -> Winner {
+    let mut hands1 = HashSet::new();
+    let mut hands2 = HashSet::new();
+
+    while !player1.is_empty() && !player2.is_empty() {
+        if !hands1.insert(player1.clone()) && !hands2.insert(player2.clone()) {
+            return Winner::Player1;
+        }
+        let card1 = player1.pop_front().unwrap();
+        let card2 = player2.pop_front().unwrap();
+        if card1 <= player1.len() && card2 <= player2.len() {
+            let mut subcards1 = player1.iter().take(card1).copied().collect();
+            let mut subcards2 = player2.iter().take(card2).copied().collect();
+            match recursive_combat(&mut subcards1, &mut subcards2) {
+                Winner::Player1 => {
+                    player1.push_back(card1);
+                    player1.push_back(card2);
+                }
+                Winner::Player2 => {
+                    player2.push_back(card2);
+                    player2.push_back(card1);
+                }
+            }
+        } else if card1 > card2 {
+            player1.push_back(card1);
+            player1.push_back(card2);
+        } else {
+            player2.push_back(card2);
+            player2.push_back(card1);
+        }
+    }
+    if player1.is_empty() {
+        Winner::Player2
+    } else {
+        Winner::Player1
+    }
+}
+
+pub fn part2(cards1: &Cards, cards2: &Cards) -> usize {
+    let mut player1: Cards = cards1.clone();
+    let mut player2: Cards = cards2.clone();
+    match recursive_combat(&mut player1, &mut player2) {
+        Winner::Player1 => score(&player1),
+        Winner::Player2 => score(&player2),
+    }
 }
--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,7 +1,6 @@
 use clap::{crate_description, App, Arg};
 use day22::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::read_to_string;
 use std::num::ParseIntError;
 use std::process::exit;
 
@@ -17,39 +16,38 @@ fn main() {
 
     println!(crate_description!());
 
-    let input = match read_input(args.value_of("INPUT").unwrap()) {
-        Ok(data) => data,
+    let (cards1, cards2) = match read_input(args.value_of("INPUT").unwrap()) {
+        Ok((cards1, cards2)) => (cards1, cards2),
         Err(err) => {
             println!("Failed to read input: {}", err);
             exit(2);
         }
     };
 
-    match part1(&input) {
-        Some(result) => println!("Part 1: {}", result),
-        None => println!("Part 1: not found"),
-    };
-
-    match part2(&input) {
-        Some(result) => println!("Part 2: {}", result),
-        None => println!("Part 2: not found"),
-    };
+    println!("Part 1: {}", part1(&cards1, &cards2));
+    println!("Part 2: {}", part2(&cards1, &cards2));
 }
 
-fn read_input(filename: &str) -> Result<Vec<i32>, String> {
-    let input_file = File::open(filename).map_err(|err| err.to_string())?;
-
-    BufReader::new(input_file)
-        .lines()
-        .zip(1..)
+fn read_input(filename: &str) -> Result<(Cards, Cards), String> {
+    let input = read_to_string(filename).map_err(|err| err.to_string())?;
+    let lines = &mut input.lines().zip(1..);
+    let cards1 = lines
+        .skip(1)
+        .take_while(|(line, _)| !line.trim().is_empty())
         .map(|(line, line_num)| {
-            line.map_err(|err| (line_num, err.to_string()))
-                .and_then(|value| {
-                    value.parse().map_err(|err: ParseIntError| {
-                        (line_num, err.to_string())
-                    })
-                })
+            line.parse().map_err(|err: ParseIntError| {
+                format!("Line {}: {}", line_num, err.to_string())
+            })
         })
-        .collect::<Result<_, _>>()
-        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
+        .collect::<Result<_, _>>()?;
+    let cards2 = lines
+        .skip(1)
+        .take_while(|(line, _)| !line.trim().is_empty())
+        .map(|(line, line_num)| {
+            line.parse().map_err(|err: ParseIntError| {
+                format!("Line {}: {}", line_num, err.to_string())
+            })
+        })
+        .collect::<Result<_, _>>()?;
+    Ok((cards1, cards2))
 }
