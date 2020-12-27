441170808bb813d5d7c374333f9b6d9d3f45ef60
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-12-27 14:32:44 +0000

    Day 25

--- day25/src/lib.rs
+++ day25/src/lib.rs
@@ -1,7 +1,24 @@
-pub fn part1(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+const SUBJECT_NUMBER: u64 = 7;
+const DIVISOR: u64 = 20201227;
+
+fn find_loop_size(pubkey: u64) -> u64 {
+    let mut loop_size = 0;
+    let mut value = 1;
+    while value != pubkey {
+        value = (value * SUBJECT_NUMBER) % DIVISOR;
+        loop_size += 1;
+    }
+    loop_size
 }
 
-pub fn part2(_input: &[i32]) -> Option<i32> {
-    unimplemented!()
+fn transform(pubkey: u64, loop_size: u64) -> u64 {
+    let mut value = 1;
+    for _ in 1..=loop_size {
+        value = (value * pubkey) % DIVISOR;
+    }
+    value
+}
+
+pub fn part1(pubkey1: u64, pubkey2: u64) -> u64 {
+    transform(pubkey2, find_loop_size(pubkey1))
 }
--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,7 +1,6 @@
 use clap::{crate_description, App, Arg};
 use day25::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::read_to_string;
 use std::num::ParseIntError;
 use std::process::exit;
 
@@ -17,7 +16,7 @@ fn main() {
 
     println!(crate_description!());
 
-    let input = match read_input(args.value_of("INPUT").unwrap()) {
+    let (key1, key2) = match read_input(args.value_of("INPUT").unwrap()) {
         Ok(data) => data,
         Err(err) => {
             println!("Failed to read input: {}", err);
@@ -25,31 +24,23 @@ fn main() {
         }
     };
 
-    match part1(&input) {
-        Some(result) => println!("Part 1: {}", result),
-        None => println!("Part 1: not found"),
-    };
-
-    match part2(&input) {
-        Some(result) => println!("Part 2: {}", result),
-        None => println!("Part 2: not found"),
-    };
+    println!("Part 1: {}", part1(key1, key2));
 }
 
-fn read_input(filename: &str) -> Result<Vec<i32>, String> {
-    let input_file = File::open(filename).map_err(|err| err.to_string())?;
-
-    BufReader::new(input_file)
+fn read_input(filename: &str) -> Result<(u64, u64), String> {
+    let input = read_to_string(filename).map_err(|err| err.to_string())?;
+    let numbers = &mut input
         .lines()
         .zip(1..)
         .map(|(line, line_num)| {
-            line.map_err(|err| (line_num, err.to_string()))
-                .and_then(|value| {
-                    value.parse().map_err(|err: ParseIntError| {
-                        (line_num, err.to_string())
-                    })
-                })
+            line.parse().map_err(|err: ParseIntError| {
+                format!("Line {}: {}", line_num, err.to_string())
+            })
         })
-        .collect::<Result<_, _>>()
-        .map_err(|(line_num, err)| format!("Line {}: {}", line_num, err))
+        .take(2)
+        .collect::<Result<Vec<_>, _>>()?;
+    match numbers.get(0..2) {
+        Some(&[key1, key2]) => Ok((key1, key2)),
+        _ => Err("Missing public key(s)".to_string()),
+    }
 }
